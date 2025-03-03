758ae64b1326f5d34d5c9ee7985bbea1dd0f7eb0
Kalashnikov-AN <huumaannnnn@gmail.com>
2025-03-03 23:31:50 +0900

    input view

--- ChatBot/src/main/java/zabsu/chatbot/chatbot/HelloController.java
+++ ChatBot/src/main/java/zabsu/chatbot/chatbot/HelloController.java
@@ -1,12 +1,16 @@
 package zabsu.chatbot.chatbot;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class HelloController {
 
@@ -29,6 +33,18 @@ public class HelloController {
     void onSendClick(ActionEvent event) {
 
     }
+    @FXML
+    private Button enter_button;
+
+    @FXML
+    void enter_buttonOnClick(ActionEvent event) throws IOException  {
+        Stage input = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1024, 680);
+        input.setTitle("Hello!");
+        input.setScene(scene);
+        input.show();
+   }
 
     @FXML
     void initialize() {
